7ccaaff1589e5229fbafca9a8e6f4695bc1f7fc6
choi <bcaca0831@gmail.com>
2023-02-16 18:16:03 +0900

    css 작업중

--- src/components/dailymenu/foodCard.js
+++ src/components/dailymenu/foodCard.js
@@ -1,16 +1,18 @@
-import React from 'react'
+/** @format */
 
-const foodCard = () => {
+import React from 'react';
+
+const FoodCard = ({ item }) => {
   return (
-    <div className='w-[190px] h-[290px]  bg-white drop-shadow-md'>
-      <div></div>
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
+    <div className=" bg-white drop-shadow-md h-[290px] py-4 px-5 rounded-2xl mx-[10px] mb-[20px] ">
+      <div>{item.img}</div>
+      <div className="flex flex-col">
+        <span>{item.title}</span>
+        <span>{item.time}</span>
+        <span>{item.kcal}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default foodCard
+export default FoodCard;
--- src/pages/DailyMenu.js
+++ src/pages/DailyMenu.js
@@ -8,16 +8,59 @@ import calender from '../assets/images/icon/icon_calendar.png';
 import diet from '../assets/images/icon/icon_diet.png';
 import addFood from '../assets/images/icon/add_food.png';
 import { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import FoodCard from '../components/dailymenu/FoodCard';
 
 const DailyMenu = () => {
-  return (
-    <div className="container w-full h-full">
-      <div className="w-full h-full">
-        <div className="w-full h-full m-auto flex">
-          <Sidebar></Sidebar>
+  const mockData = [
+    {
+      img: '이미지1',
+      title: '그릭요거트1',
+      time: 'am:08:31',
+      kcal: '300kcal',
+    },
+    {
+      img: '이미지2',
+      title: '그릭요거트2',
+      time: 'am:09:31',
+      kcal: '300kcal',
+    },
+    {
+      img: '이미지3',
+      title: '그릭요거트3',
+      time: 'am:010:31',
+      kcal: '300kcal',
+    },
+    {
+      img: '이미지4',
+      title: '그릭요거트4',
+      time: 'am:10:31',
+      kcal: '300kcal',
+    },
+    {
+      img: '이미지5',
+      title: '그릭요거트5',
+      time: 'am:11:31',
+      kcal: '300kcal',
+    },
+    {
+      img: '이미지6',
+      title: '그릭요거트6',
+      time: 'pm:12:31',
+      kcal: '300kcal',
+    },
+  ];
 
-          <div className="w-full p-8 ml-8 mb-[20px] rounded-2xl border bg-white drop-shadow-md">
-            <div className="flex justify-between mb-8">
+  return (
+    <>
+      <Helmet>
+        <title>마이페이지</title>
+      </Helmet>
+      <div className="container w-full h-full m-auto flex">
+        <Sidebar></Sidebar>
+        <div className="w-full">
+          <div className=" ml-8 mb-[20px] rounded-2xl border bg-white drop-shadow-md">
+            <div className="flex justify-between p-8">
               <div className="flex">
                 <img
                   src={food}
@@ -30,14 +73,18 @@ const DailyMenu = () => {
                 <img src={calender} alt="food" />
               </button>
             </div>
-            <div>
-              <div className="bg-addfood bg-no-repeat bg-contain w-[190px] h-[290px]" />
+            <div className="mx-4">
+              <div className=" grid grid-cols-4 ">
+                <div className="bg-[#BDD1D4] bg-center bg-addfood bg-no-repeat  h-[290px] rounded-2xl mx-[10px]" />
+                {mockData.map((item, index) => {
+                  return <FoodCard item={item} key={index} />;
+                })}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-
 export default DailyMenu;
