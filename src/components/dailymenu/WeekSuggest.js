c6a6206bf3f77e3315ea5b17aed238eb7f5eb2cc
choi <bcaca0831@gmail.com>
2023-02-28 16:32:21 +0900

    axios 연동중

--- src/components/dailymenu/WeekSuggest.js
+++ src/components/dailymenu/WeekSuggest.js
@@ -5,19 +5,34 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 const WeekSuggest = () => {
-  const [suggestList, setSuggestList] = useState();
+  const [suggestList, setSuggestList] = useState([]);
   useEffect(() => {
     axios
       .get('http://192.168.0.16:9876/api/suggest/suggest/week?token=token1')
       .then((res) => {
-        console.log(res);
+        console.log(res.data.data);
+        setSuggestList(res.data.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+  console.log(suggestList);
 
-  return <div></div>;
+  const suggesSug = suggestList.map((item, index) => {
+    return (
+      <div key={index}>
+        <span>{item.date}</span>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <p>뭐야</p>
+      <p>{suggesSug}</p>
+    </div>
+  );
 };
 
 export default WeekSuggest;
